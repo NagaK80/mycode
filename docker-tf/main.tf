05773cbead0234a638e39a220df5288fcd95a248
NagaK80 <nvmkkadiyala@hotmail.com>
2024-02-12 07:17:04 +0000

    Terraform & Variables 2

--- docker-tf/main.tf
+++ docker-tf/main.tf
@@ -32,7 +32,7 @@ resource "docker_container" "nginx" {
 
 resource "docker_container" "nginx" {
   image = docker_image.nginx.image_id
-  name  = var.continer_name
+  name  = var.container_name
   ports {
     internal = 80
     external = 2224
